c6a634e04f9da92379f2694ce545f34beae96b68
thomasescartin <thomas.escartin@gmail.com>
2026-09-14 12:13:34 +0200

    Changement de createConnection en createPool.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const dotenv = require("dotenv/config");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
